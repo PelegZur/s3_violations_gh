e43894a338da88e39d0153b44dead36b5a163023
PelegZur <57677867+PelegZur@users.noreply.github.com>
2023-03-30 16:34:00 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,5 +4,9 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "prefix-data"
+  
+  
+  
+  
   force_destroy = true
 }
