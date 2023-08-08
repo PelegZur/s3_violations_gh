0db635a37209824cdb95de673d75fb6cf4bdeced
PelegZur <57677867+PelegZur@users.noreply.github.com>
2023-08-08 16:12:36 +0300

    Delete s3_copy.tf

--- s3_copy.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "data-2" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "prefix-data-2"
-  force_destroy = true
-}
